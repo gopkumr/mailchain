378e464fcdb6f859dcf91b22c43723f0c5f52d36
gopkumr <gopakumar.r@live.com>
2018-11-27 21:03:02 +1100

    Added client side call to register user

--- src/js/app.js
+++ src/js/app.js
@@ -3,61 +3,60 @@ App = {
   contracts: {},
 
   init: function() {
-    // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
-
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
-
-        petsRow.append(petTemplate.html());
-      }
-    });
-
+    $("#btnSubmit").attr('disabled','disabled');;
     return App.initWeb3();
+
   },
 
   initWeb3: function() {
-    /*
-     * Replace me...
-     */
+    if (typeof web3 !== 'undefined') {
+      // If a web3 instance is already provided by Meta Mask.
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // Specify default instance if no web3 instance provided
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      web3 = new Web3(App.web3Provider);
+    }
 
     return App.initContract();
   },
 
   initContract: function() {
-    /*
-     * Replace me...
-     */
+    $.getJSON("EmailServer.json", function(emailServer) {
+      // Instantiate a new truffle contract from the artifact
+      App.contracts.EmailServer = TruffleContract(emailServer);
+      // Connect provider to interact with contract
+      App.contracts.EmailServer.setProvider(App.web3Provider);
+    });
 
     return App.bindEvents();
   },
 
   bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
+   $("#btnSubmit").removeAttr('disabled');
+   $("#btnSubmit").click(App.registerUser);
+
+   web3.eth.getCoinbase(function(err, account) {
+    if (err === null) {
+      App.account = account;
+      $("#accountInfo").html("Your Account: " + account);
+    }
+  });
   },
 
-  markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
+  registerUser: function() {
+    var emailServerInstance;
+    var userName=$("#userName").val();
+   App.contracts.EmailServer.deployed().then(function(instance){
+      emailServerInstance=instance;
+      return emailServerInstance.addUser(userName);
+   }).then(function(result){
+       return emailServerInstance.userCount();
+   }).then(function(result){
+      alert(JSON.stringify(result));
+   });
   },
-
-  handleAdopt: function(event) {
-    event.preventDefault();
-
-    var petId = parseInt($(event.target).data('id'));
-
-    /*
-     * Replace me...
-     */
-  }
 
 };
 
